0fd7c293c359e18f270f137c1d4b96ebd7e65696
F3licia <romeofelicia@gmail.com>
2021-04-29 17:41:48 +0200

    manca bonus

---
+++ myScriptVue.js
@@ -0,0 +1,21 @@
+/*Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
+
+
+const app = new Vue(
+    {
+        el: '#vueContainer',
+        data: {
+            message:"Welcome!",
+            images:"",
+            classi:["hide",]
+            },
+        methods: {
+            miaFunzione() { 
+               this.classi = "display";
+            }    
+        }
+    }
+);
